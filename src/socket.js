4c3b1e1fad7290e80a80ac2649cb1c034ed22140
sauheiwong <sauheiwong@gmail.com>
2025-09-07 23:13:47 -0700

    edit socket url

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = 'http://localhost:4000';
+const URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
 
 const socket = io(URL, {
     autoConnect: false,
